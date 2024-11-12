96d776604ffaee32b9c6fcb0dffdaafd8327c0a1
ARIHORonald <ronaldariho93@gmail.com>
2024-11-12 18:58:32 +0300

    Update conn.php

--- conn/conn.php
+++ conn/conn.php
@@ -6,6 +6,7 @@ $password = "";
 $db = "expense_tracker_db";
 
 
+
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
